adcf7fd37e88c1e8539fd843e0334822776f4ea3
maksim3382 <maksimlaskov961@gmail.com>
2026-05-13 12:49:39 +0300

    Update photos.js

--- photos.js
+++ photos.js
@@ -2,3 +2,15 @@ const memories = [
   { src: 'assets/memory1.jpg', title: 'Первый выезд', description: 'Тот самый момент, когда всё началось.' },
   { src: 'assets/memory2.jpg', title: 'Команда ПЦ', description: 'Лучшие люди, с которыми пройдено немало.' }
 ];
+const memories = [
+  { 
+    src: 'assets/photo1.jpg', 
+    title: 'Название фото', 
+    description: 'Описание момента' 
+  },
+  { 
+    src: 'assets/new-photo.jpg', // Название твоего нового файла
+    title: 'Мой новый кадр', 
+    description: 'Текст, который появится при клике' 
+  } // Если это последнее фото, запятую после } можно не ставить
+];
